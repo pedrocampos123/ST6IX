52ed807c723afc56fda8f3a3f9aef314c7396b28
PedroCampos_ <pedroarturocampos@gmail.com>
2021-01-17 23:28:06 -0600

    index.js
    sa

--- index.js
+++ index.js
@@ -12,12 +12,48 @@ client.on("ready", () => {
 client.on("guildMemberAdd", (member) => {
     try {
         //let canal = client.channels.cache.get('432763750316113932');
-		const canal = member.channels.cache.find(canal => canal.name === "lobby");
-		
-		if(!canal) return;
-		
+        const canal = member.channels.cache.find(canal => canal.name === "lobby");
+
+        if (!canal) return;
+
         canal.send(`Hey **${member.user.username}**, welcome to ${client.user.username} :tada::hugging:`);
     } catch (error) {}
 });
 
-client.login(config.token);
+client.on("message", async message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+
+    let usuario = message.author;
+
+    switch (command) {
+        case "saludo":
+            return message.channel.send(`Hola ${usuario.username}`);
+            break;
+        case "helpbot":
+            return message.channel.send("!saludo\n!information");
+            break;
+        case "information":
+            const embed = new Discord.RichEmbed()
+                .setAuthor(`ST6IX`, client.user.avatarURL)
+                .addField(`User`, `${usuario.username}`, true)
+                .addField(`Servers`, `${client.guilds.size}`, true)
+                .addField(`Users`, `${client.users.size}`, true)
+                .addField(`Discord`, `[ST6IX/discord](https://discord.gg/hKa8HrEH)`, true)
+                .addField(`Invite`, `[ST6IX/discord](https://discord.gg/hKa8HrEH)`, true)
+                .addField(`Developer`, `ARTURO_#1583`, true)
+                .addField(`YouTube`, "https://www.youtube.com/channel/UCBqpmuhsKpwh8IX-ge6kCbQ", true)
+                .addField(`Version`, `1.0`, true)
+                .setTimestamp()
+                .setColor("RANDOM");
+            message.channel.send(embed);
+            break;
+        default:
+            return message.channel.send("comando invalido, utilizar el comando !helpbot");
+            break;
+    }
+});
+
+client.login(process.env.token);
